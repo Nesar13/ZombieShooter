c43f99a32698825410fb273faf13e7a0465d3654
Nesar Ahmed <nesar13@hotmail.com>
2020-09-19 22:19:16 -0400

    Added WeaponsZoom.cs

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float zoomedOutFOV = 60f;
+    [SerializeField] float zoomedInFOV = 20f;
+
+    bool zoomedInToggle = false; 
+
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        // right click mouse - 0 is for left click
+        if (Input.GetMouseButton(1))
+        {   
+            // we want to toggle the fov 
+            if (zoomedInToggle == false)
+            {
+                zoomedInToggle = true;
+                fpsCamera.fieldOfView = zoomedInFOV; 
+            }
+            else
+            {
+                zoomedInToggle = false;
+                fpsCamera.fieldOfView = zoomedOutFOV; 
+            }
+        }
+    }
+}
